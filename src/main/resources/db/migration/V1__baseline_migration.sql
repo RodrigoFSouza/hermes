7a86cb306943bf684def874bbb54799c8ce68cea
Rodrigo Souza <roferreira122@gmail.com>
2023-04-20 13:25:02 -0300

    alterando banco de dados de testes para banco em memoria h2

--- src/main/resources/db/migration/V1__baseline_migration.sql
+++ src/main/resources/db/migration/V1__baseline_migration.sql
@@ -1,5 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE usuarios (
      id uuid NOT NULL,
      nome  VARCHAR(250)  NOT NULL,
@@ -7,12 +5,6 @@ CREATE TABLE usuarios (
      senha VARCHAR(250)  NOT NULL,
      CONSTRAINT usuarios_pkey PRIMARY KEY (id)
 );
-
-INSERT INTO usuarios (id, nome, email, senha) VALUES
-(uuid_generate_v4(), 'rodrigo','rodrigo@cronos.com.br', 'teste123'),
-(uuid_generate_v4(), 'pedro','pedro@cronos.com.br', 'teste321'),
-(uuid_generate_v4(), 'joao','joao@cronos.com.br', 'teste999'),
-(uuid_generate_v4(), 'maria','maria@cronos.com.br', 'teste333');
 
 CREATE TABLE lancamentos (
      id uuid NOT NULL,
